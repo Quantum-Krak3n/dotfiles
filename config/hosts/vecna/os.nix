b3173cbf4b1ac4a4eaa7c7d0ef81eadc5c79253a
Matthew Murphy <matt@barbermurphy.id.au>
2026-01-14 16:19:59 +1100

    Add Nvidia Drivers

--- config/hosts/vecna/os.nix
+++ config/hosts/vecna/os.nix
@@ -12,6 +12,8 @@
   ];
 
   networking.hostName = "vecna";
+  hardware.nvidia.open = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
